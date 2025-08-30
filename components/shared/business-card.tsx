6eb549f1dd441f0e06a4aedbe59b1be473f6e7d8
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-08-30 12:19:08 +0600

    claim modify

--- components/shared/business-card.tsx
+++ components/shared/business-card.tsx
@@ -1,64 +1,78 @@
 "use client";
 
 import Image from "next/image";
-import { Star, MapPin } from "lucide-react";
+import { Star } from "lucide-react";
+import Link from "next/link";
 
-export interface Business {
-    _id: string; // Better to use a unique ID!
-    name: string;
-    image: string[];
-    status?: string;
-    rating: number | null;
-    reviewsCount: number;
-    address: string;
+interface BusinessItem {
+  email: string;
+  name: string;
+  image: string;
 }
 
+interface Service {
+  newInstrumentName: string;
+}
+
+interface Business {
+  _id: string;
+  businessInfo: BusinessItem;
+  services: Service[];
+}
+
+
 interface BusinessCardProps {
-    business: Business;
+  business: Business;
 }
 
 export default function BusinessCard({ business }: BusinessCardProps) {
-    return (
-        <div className="p-4  rounded-md shadow-[0px_2px_12px_0px_#003D3914]">
-            <div className="relative mb-3">
-                <Image
-                    src={business.image[0] ?? business.image}
-                    alt={business.name}
-                    width={300}
-                    height={300}
-                    className="w-full aspect-[5/4] object-cover rounded-md border"
-                />
-                <p
-                    className={`absolute top-2 right-2 px-4 py-1 rounded-xl ${business.status === "Published"
-                        ? "text-green-500"
-                        : business.status === "Pending"
-                            ? "text-[#E38441] bg-[#E384411F]"
-                            : "text-[#E24040] bg-[#E240401F]"
-                        }`}
-                >
-                    {business.status}
-                </p>
-            </div>
+  return (
+    <Link href={`/search-result/${business?._id}`}>
+      <div className="bg-white rounded-lg border border-gray-100 shadow-lg p-6 h-full">
+        <div className="flex flex-col gap-5 h-full">
+          {/* Profile Image */}
+          <div className="flex-shrink-0 overflow-hidden rounded-lg">
+            <Image
+              src={business?.businessInfo?.image[0] || "/placeholder.svg"}
+              alt={"business.png"}
+              width={1000}
+              height={1000}
+              className="rounded-lg object-cover w-full h-[250px] hover:scale-105 transition"
+            />
+          </div>
 
-            <div className="space-y-1">
-                <h4 className="text-lg font-semibold">{business.name}</h4>
+          {/* Content */}
+          <div className="flex-1 flex flex-col">
+            <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between gap-4">
+              <div className="flex-1">
+                <h3 className="text-lg font-semibold text-gray-900">
+                  {business?.businessInfo?.name}
+                </h3>
 
-                <div className="flex items-center gap-2 text-[#485150]">
-                    <Star
-                        className="h-5 w-5"
-                        fill={business.reviewsCount > 0 ? "#F4C321" : "#E7E9E9"}
-                        stroke="none"
-                    />
-                    {business.reviewsCount === 0 || business.rating === null
-                        ? "Not Rated"
-                        : `${business.rating} (${business.reviewsCount})`}
+                {/* Rating */}
+                <div className="flex items-center gap-1 my-3">
+                  <Star className="h-4 w-4 fill-yellow-400 text-yellow-400" />
+                  <span className="text-sm font-medium">{"3.7"}</span>
                 </div>
 
-                <p className="text-sm text-gray-500 flex items-center gap-2">
-                    <MapPin className="h-5 w-5" />
-                    {business.address}
-                </p>
+                {/* Services */}
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-wrap items-center gap-2 mb-2">
+                    {business?.services?.map((service, index) => (
+                      <button
+                        className="h-[48px] px-5 rounded-lg bg-[#F8F8F8]"
+                        key={index}
+                      >
+                        {service?.newInstrumentName}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </Link>
+  );
 }
