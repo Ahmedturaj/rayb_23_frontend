732c7e9889d060560222cfbe6b857530acf255f7
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-09-29 16:39:54 +0600

    all issue resolve

--- app/(website)/search-result/_component/BusinessCard.tsx
+++ app/(website)/search-result/_component/BusinessCard.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Star } from "lucide-react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,11 +22,11 @@ interface Service {
 interface Business {
   _id: string;
   businessInfo: BusinessItem;
+  review?: string;
   services: Service[];
 }
 
 const BusinessCard = ({ business }: { business: Business }) => {
-
   return (
     <div className="bg-white rounded-lg shadow-[0px_2px_12px_0px_#003d3924] p-4 lg:p-6">
       <div className="flex flex-col sm:flex-row items-start gap-4 lg:gap-5">
@@ -44,10 +45,15 @@ const BusinessCard = ({ business }: { business: Business }) => {
               <h3 className="text-lg font-semibold text-gray-900">
                 {business.businessInfo?.name}
               </h3>
-              <div className="flex items-center gap-1 my-2 lg:my-3">
-                <Star className="h-4 w-4 fill-yellow-400 text-yellow-400" />
-                <span className="text-sm font-medium">3.7</span>
+
+              <div className="my-3">
+                <Rating
+                  style={{ maxWidth: 100 }}
+                  value={business.review ? business.review.length : 0}
+                  readOnly
+                />
               </div>
+
               <div className="flex flex-col gap-2">
                 <div className="flex flex-wrap items-center gap-2 mb-2">
                   {business?.services?.map((service, index) => (
